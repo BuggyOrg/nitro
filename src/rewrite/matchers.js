ab9e76e256b55f6efb414140989967398bd48806
leMaik <an@maik.tk>
2016-05-05 18:54:13 +0200

    (refactor) Normalize matcher output. Add the previously removed rewrite rule for constant number to string conversions.

--- src/rewrite/matchers.js
+++ src/rewrite/matchers.js
@@ -13,13 +13,13 @@ export function byIdAndInputs (id, inputs) {
   return (graph, n) => {
     const node = graph.node(n)
     if (node.id === id) {
-      const match = {}
+      const match = { node: n, inputs: {} }
       const isMatch = Object.keys(inputs).every((inputPort) => {
         let predecessors = atomicPredecessorsOutPort(graph, n, inputPort)
         if (predecessors.length === 1) {
           return predecessors.every(({node, port}) => {
-            match[inputPort] = inputs[inputPort](graph, node)
-            return match[inputPort] !== false
+            match.inputs[inputPort] = inputs[inputPort](graph, node)
+            return match.inputs[inputPort] !== false
           })
         } else {
           return false
@@ -36,8 +36,8 @@ export function constantNode (outputAlias) {
   return (graph, n) => {
     const node = graph.node(n)
     if (node.id === 'math/const') {
-      const match = {}
-      match[outputAlias] = { node: n, port: Object.keys(node.outputPorts)[0] }
+      const match = { node: n, outputs: {} }
+      match.outputs[outputAlias] = Object.keys(node.outputPorts)[0]
       return match
     } else {
       return false
--- src/rewrite/rules.js
+++ src/rewrite/rules.js
@@ -4,7 +4,7 @@ import rule from './rewriteRule'
 
 export const replaceConstantAddition = rule(
   match.byIdAndInputs('math/add', { s1: match.constantNode('s1'), s2: match.constantNode('s2') }),
-  replace.withNode((graph, node, { s1, s2 }) => {
+  replace.withNode((graph, node, match) => {
     return {
       node: {
         id: 'math/const',
@@ -13,11 +13,32 @@ export const replaceConstantAddition = rule(
         outputPorts: { output: 'number' },
         atomic: true,
         path: [],
-        params: { value: graph.node(s1.s1.node).params.value + graph.node(s2.s2.node).params.value },
+        params: { value: graph.node(match.inputs.s1.node).params.value + graph.node(match.inputs.s2.node).params.value },
         name: 'const'
       },
       rewriteOutputPorts: {
         sum: 'output'
+      }
+    }
+  })
+)
+
+export const replaceConstantNumberToString = rule(
+  match.byIdAndInputs('translator/number_to_string', { input: match.byIdAndInputs('math/const', {}) }),
+  replace.withNode((graph, node, match) => {
+    return {
+      node: {
+        id: 'string/const',
+        version: '0.2.0',
+        inputPorts: {},
+        outputPorts: { output: 'string' },
+        atomic: true,
+        path: [],
+        params: { value: `${graph.node(match.inputs.input.node).params.value}` },
+        name: 'const'
+      },
+      rewriteOutputPorts: {
+        output: 'output'
       }
     }
   })
