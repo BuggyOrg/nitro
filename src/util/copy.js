b58eb611af853b317d9eb98262a5063701114e49
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2016-05-27 15:54:12 +0200

    (feat) Add a function to copy nodes.

---
+++ src/util/copy.js
@@ -0,0 +1,28 @@
+import _ from 'lodash'
+
+function copyNodeInto (graph, node, target) {
+  const nodeCopy = `${node}_${_.uniqueId('copy_')}`
+  const newNodeValue = _.cloneDeep(graph.node(node))
+
+  graph.setNode(nodeCopy, newNodeValue)
+  graph.setParent(nodeCopy, target)
+
+  const children = {}
+  graph.children(node).forEach((child) => {
+    children[child] = copyNodeInto(graph, child, nodeCopy)
+  })
+
+  graph.edges().forEach((e) => {
+    const source = children[e.v]
+    const target = children[e.w]
+    if (source && target) {
+      graph.setEdge(source, target, _.cloneDeep(graph.edge(e)), `${e}_${_.uniqueId('copy_')}`)
+    }
+  })
+
+  return nodeCopy
+}
+
+export function copyNode (graph, node) {
+  return copyNodeInto(graph, node, graph.parent(node))
+}
