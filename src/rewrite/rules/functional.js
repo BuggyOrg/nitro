977c99d88b7bdd2aefedd9a73dff79cc95ad5eba
leMaik <an@maik.tk>
2016-06-05 14:11:24 +0200

    (feat) Rewrite functional/call instead of functional/apply.

--- src/rewrite/rules/functional.js
+++ src/rewrite/rules/functional.js
@@ -2,19 +2,16 @@ import _ from 'lodash'
 import { walk } from '@buggyorg/graphtools'
 import { rule, match, replace } from '../rewrite'
 import { copyNode } from '../../util/copy'
-import { createEdgeToEachSuccessor, createEdgeFromEachPredecessor, movePredecessorsInto, deepRemoveNode, createEdge } from '../../util/rewrite'
+import { createEdgeToEachSuccessor, movePredecessorsInto, deepRemoveNode, createEdge } from '../../util/rewrite'
 import { realPredecessors } from '../../util/realWalk'
 
-export const replaceNonRecursiveApply = rule(
-  match.byIdAndInputs('functional/apply', {
-    fn: match.lambda({ recursive: false, sideeffects: false }),
-    value: match.any()
+export const replaceNonRecursiveCall = rule(
+  match.byIdAndInputs('functional/call', {
+    fn: match.lambda({ recursive: false, sideeffects: false })
   }),
   (graph, node, match) => {
     const lamdaImpl = copyNode(graph, graph.children(match.inputs.fn.node)[0])
     graph.setParent(lamdaImpl, graph.parent(node))
-
-    createEdgeFromEachPredecessor(graph, { node: match.node, port: 'value' }, lamdaImpl)
     createEdgeToEachSuccessor(graph, lamdaImpl, node)
     graph.removeNode(node)
   }
