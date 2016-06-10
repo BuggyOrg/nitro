b873b636e40696f882584dfeac4a58aab7675af1
Maik Marschner <an@maik.tk>
2016-06-10 20:45:03 +0200

    (feat) Support rules that are exported as an array of rules.

--- src/rewrite/rules/index.js
+++ src/rewrite/rules/index.js
@@ -1,24 +1,26 @@
+import _ from 'lodash'
 import * as mathRules from './math'
 import * as logicRules from './logic'
 import * as functionalRules from './functional'
 import * as compoundRules from './compounds'
 
 function applyAndReturnName (functions) {
-  return Object.keys(functions).map((r) =>
-    (...args) => {
-      const applied = functions[r].apply(undefined, args)
+  return Object.keys(functions).map((r) => {
+    const fn = _.isArray(functions[r]) ? functions[r] : [functions[r]]
+    return fn.map((fn) => (...args) => {
+      const applied = fn.apply(undefined, args)
       if (applied) {
-        return (functions[r].meta ? functions[r].meta.name : null) || r
+        return (fn.meta ? fn.meta.name : null) || r
       } else {
         return false
       }
-    }
-  )
+    })
+  })
 }
 
-export default [
-  ...applyAndReturnName(mathRules),
-  ...applyAndReturnName(logicRules),
-  ...applyAndReturnName(functionalRules),
-  ...applyAndReturnName(compoundRules)
-]
+export default _.flattenDeep([
+  applyAndReturnName(mathRules),
+  applyAndReturnName(logicRules),
+  applyAndReturnName(functionalRules),
+  applyAndReturnName(compoundRules)
+])
