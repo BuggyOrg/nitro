1ec5d5e5f6a3d6b22ea9628da9dbf3221fcf209c
leMaik <an@maik.tk>
2016-06-14 18:15:34 +0200

    (feat) Include tail calls and predicates in the tail recursive compound matcher's output.

--- src/rewrite/rules/tailrecursion/index.js
+++ src/rewrite/rules/tailrecursion/index.js
@@ -15,26 +15,37 @@ export function matchTailRecursiveCompound (graph, n) {
     return false
   }
 
+  const predicates = [] // collected while walking up the `logic/mux` chain
+
   // in Buggy, a compound is tail-recursive if it ends with a chain of `logic/mux` nodes and recursive calls only
   // occur in this chain
   const walkMuxChain = (node, port) => {
     return walk.predecessor(graph, node, port).map((p) => {
       const predecessor = graph.node(p.node)
       if (predecessor.id === 'logic/mux') {
+        predicates.push(walk.predecessor(graph, p, 'control')[0])
         return [walkMuxChain(p.node, 'input1'), walkMuxChain(p.node, 'input2')]
       } else if (predecessor.id === compoundNode.id) {
         return p.node
       }
     })
   }
-  const tailcalls = _.flattenDeep(walkMuxChain(n, Object.keys(compoundNode.outputPorts || {})[0]))
+  const tailcalls = _.without(_.flattenDeep(walkMuxChain(n, Object.keys(compoundNode.outputPorts || {})[0])), n)
 
   // TODO more validation, every mux may only be used once
   // every recursive call should be a tail call and not be used otherwise
-  return _.difference(recursiveCalls, tailcalls).length === 0 && recursiveCalls.every((c) => {
+  if (_.difference(recursiveCalls, tailcalls).length === 0 && recursiveCalls.every((c) => {
     const call = graph.node(c)
     return Object.keys(call.outputPorts).every((port) => walk.successor(graph, c, port).every((successor) => {
       return graph.node(successor.node).id === compoundNode || graph.node(successor.node).id === 'logic/mux'
     }))
-  })
+  })) {
+    return {
+      node: n,
+      predicates: _.uniq(predicates),
+      tailcalls: _.uniq(tailcalls)
+    }
+  } else {
+    return false
+  }
 }
