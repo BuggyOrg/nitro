c6c3966aaf1530e4d61ca3527b2683395ad217d3
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2016-07-15 14:49:30 +0200

    (feat) Support creating successors with the subgraph creator.

--- src/util/subgraphCreator.js
+++ src/util/subgraphCreator.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import { createEdge, setNodeIn } from './rewrite'
 
-export function createSubgraph (graph, context, subgraph) {
+export function createSubgraph (graph, context, subgraph, type = 'predecessor') {
   let node, port
   if (_.isObject(subgraph.node)) {
     node = `subgraph_node_${_.uniqueId()}`
@@ -16,16 +16,25 @@ export function createSubgraph (graph, context, subgraph) {
     port = subgraph.port
   } else {
     const nodeValue = graph.node(node)
-    const outputPorts = _.keys(nodeValue.outputPorts || {})
-    if (outputPorts.length === 1) {
-      port = outputPorts[0]
-    } else if (outputPorts.length > 1) {
-      throw new Error(`${node} has more than one output port, cannot select one automatically`)
-    } // port is intentionally left undefined if there are no output ports
+    if (type === 'predecessor') {
+      const outputPorts = _.keys(nodeValue.outputPorts || {})
+      if (outputPorts.length === 1) {
+        port = outputPorts[0]
+      } else if (outputPorts.length > 1) {
+        throw new Error(`${node} has more than one output port, cannot select one automatically`)
+      } // port is intentionally left undefined if there are no output ports
+    } else {
+      const inputPorts = _.keys(nodeValue.inputPorts || {})
+      if (inputPorts.length === 1) {
+        port = inputPorts[0]
+      } else if (inputPorts.length > 1) {
+        throw new Error(`${node} has more than one input port, cannot select one automatically`)
+      } // port is intentionally left undefined if there are no output ports
+    }
   }
 
   const predecessors = _.map(subgraph.predecessors || {}, (subgraph, inputPort) => {
-    const predecessor = createSubgraph(graph, context, subgraph)
+    const predecessor = createSubgraph(graph, context, subgraph, 'predecessor')
     if (predecessor.port) {
       createEdge(graph, predecessor, { node, port: inputPort })
     } else {
@@ -34,7 +43,17 @@ export function createSubgraph (graph, context, subgraph) {
     return predecessor
   })
 
-  return { node, port, predecessors }
+  const successors = _.map(subgraph.successors || {}, (subgraph, outputPort) => {
+    const successor = createSubgraph(graph, context, subgraph, 'successor')
+    if (successor.port) {
+      createEdge(graph, { node, port: outputPort }, successor)
+    } else {
+      throw new Error(`${successor.node} has no input port`)
+    }
+    return successor
+  })
+
+  return { node, port, predecessors, successors }
 }
 
 export default createSubgraph
